59df51277c86d7fc50184bec8720c046fc816e1f
MRPartik <loyolapat04@gmail.com>
2019-01-15 06:54:49 +0800

    FIxing Codes

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::post('/product/actDeact','manageProduct@actDeact');
+Route::post('/product/appDisapprove','manageProduct@appDisapprove');
+Route::post('/product/ProductVar','manageProduct@ProductVar');
+Route::get('/product/showProductVar/{id}','manageProduct@showProductVar');
+Route::post('/product/deleteAllProductVar','manageProduct@deleteAllProductVar');
+Route::post('/category/actDeact','manageProductCategory@actDeact');
+Route::get('/product/category/create/{type}','manageProductCategory@create');
+Route::post('/tax/actDeact','manageTax@actDeact');
+
+
+Route::resource('/product/list', 'manageProduct');
+Route::resource('/product/category', 'manageProductCategory');
+Route::resource('/tax', 'manageTax');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
